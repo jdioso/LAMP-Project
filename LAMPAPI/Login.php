1b37fcbb12c78bda4759b86f906f5a6ee57df5cb
DavidUmanzor <DavidU00@hotmail.com>
2023-08-27 13:38:19 -0400

    Update html

--- LAMPAPI/Login.php
+++ LAMPAPI/Login.php
@@ -15,7 +15,7 @@
 	else
 	{
 		$stmt = $conn->prepare("SELECT ID,firstName,lastName FROM Users WHERE Login=? AND Password =?");
-		$stmt->bind_param("ss", $inData["login"], $inData["password"]);
+		$stmt->bind_param("ss", $inData["loginName"], $inData["loginPassword"]);
 		$stmt->execute();
 		$result = $stmt->get_result();
 
